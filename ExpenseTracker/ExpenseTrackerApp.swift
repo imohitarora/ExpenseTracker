df69413f87c3d4c095b2170052cbe00a74ee9595
Mohit Arora <me@aboutmohit.com>
2024-04-14 19:26:11 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Mohit Arora on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
